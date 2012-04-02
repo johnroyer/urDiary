217500d535abbe981ee17974088d92fd67a7df25
Zero <john.royer@gmail.com>
2012-04-02 10:50:06 +0800

    Arrange Table Columns
    Put special date, anniversary, birthday and some columns in the
    front.

--- searchdialog.cpp
+++ searchdialog.cpp
@@ -15,24 +15,25 @@ SearchDialog::SearchDialog(QWidget *parent, QSqlDatabase *dbConn) :
 
     // Set table view
     model = new QSqlQueryModel(this);
-    model->setQuery("select * from diary", dbConn->database());
-    model->setHeaderData(0, Qt::Horizontal, "ID");
-    model->setHeaderData(1, Qt::Horizontal, "日期");
-    model->setHeaderData(2, Qt::Horizontal, "成功日記");
-    model->setHeaderData(3, Qt::Horizontal, "人生的夢/興趣");
-    model->setHeaderData(4, Qt::Horizontal, "家庭");
-    model->setHeaderData(5, Qt::Horizontal, "金錢");
-    model->setHeaderData(6, Qt::Horizontal, "健康");
-    model->setHeaderData(7, Qt::Horizontal, "人際");
-    model->setHeaderData(8, Qt::Horizontal, "感情");
-    model->setHeaderData(9, Qt::Horizontal, "工作/學習");
-    model->setHeaderData(10, Qt::Horizontal, "紀念日");
-    model->setHeaderData(11, Qt::Horizontal, "生日");
-    model->setHeaderData(12, Qt::Horizontal, "命運之日");
-    model->setHeaderData(13, Qt::Horizontal, "邂逅");
-    model->setHeaderData(14, Qt::Horizontal, "什麼日子");
-    model->setHeaderData(15, Qt::Horizontal, "天氣");
-    model->setHeaderData(16, Qt::Horizontal, "起床");
+    model->setQuery("select date, getup, anniversary, birth, fate, meet, specialDate, "
+                    "field1, field2, field3, field4, field5, field6, "
+                    "field7, field8 from diary", dbConn->database());
+
+    model->setHeaderData(0, Qt::Horizontal, "日期");
+    model->setHeaderData(1, Qt::Horizontal, "起床");
+    model->setHeaderData(2, Qt::Horizontal, "紀念日");
+    model->setHeaderData(3, Qt::Horizontal, "生日");
+    model->setHeaderData(4, Qt::Horizontal, "命運之日");
+    model->setHeaderData(5, Qt::Horizontal, "邂逅");
+    model->setHeaderData(6, Qt::Horizontal, "什麼日子");
+    model->setHeaderData(7, Qt::Horizontal, "成功日記");
+    model->setHeaderData(8, Qt::Horizontal, "人生的夢/興趣");
+    model->setHeaderData(9, Qt::Horizontal, "家庭");
+    model->setHeaderData(10, Qt::Horizontal, "金錢");
+    model->setHeaderData(11, Qt::Horizontal, "健康");
+    model->setHeaderData(12, Qt::Horizontal, "人際");
+    model->setHeaderData(13, Qt::Horizontal, "感情");
+    model->setHeaderData(14, Qt::Horizontal, "工作/學習");
 
     ui->tableView->setModel(model);
     ui->tableView->show();
@@ -51,7 +52,7 @@ SearchDialog::~SearchDialog()
 
 void SearchDialog::on_tableView_doubleClicked(const QModelIndex &index)
 {
-    QString str = model->data( model->index(index.row(), 1 ) ).toString();
+    QString str = model->data( model->index(index.row(), 0 ) ).toString();
     int y = str.mid(0,4).toInt();
     int m = str.mid(4,2).toInt();
     int d = str.mid(6,2).toInt();
